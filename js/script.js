d14a7eba3e5b357a140d731d2729b7c2e15fc56b
mrmoi <mlmart16@asu.edu>
2016-01-21 00:45:20 -0700

    jan 21 2016

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $('.carousel').carousel({
     interval: 3000
 });
+
+$(window).scroll(function() {
+    $('#alertDiv').each(function(){
+        var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+300) {
+				$(this).addClass("slideUp");
+			}
+    });
+});
